a25e1719c7cc8cbd401ec8dcbe599e36deeca66f
h.clewlow <Harry.Clewlow.1993@gmail.com>
2022-05-04 23:47:48 +0100

    Version 0.0.8 //While Loop Version + Change To Recursion If Time

--- src/main/java/net/plus/supermarket/Basket.java
+++ src/main/java/net/plus/supermarket/Basket.java
@@ -63,57 +63,71 @@ public class Basket {
         return new ArrayList<>(new LinkedHashSet<>(itemsList));
     }
 
-    //COMMENT FRM HERE
-
-
     //Print Out The Basket & Detailed Information
     public String basketCheckOut() {
+        //Variables for price and count
         Float itemPrice = 0F;
-        int maxValue = 0;
-        int itemCount = 0;
-        
+        int specialPriceQuantityNeeded = 0;
+        int itemQuantity = 0;
         Float totalPriceWithSpecialPrice = 0F;
         Float totalPriceWithOutSpecialPrice = 0F;
 
+        //StringBuilder For Printing
         StringBuilder sb = new StringBuilder();
         sb.append("      ===========================" + "\n            *** BASKET ****" + "\n      ===========================");
 
+        //Getting HashMap Of Quantities
         Map<String, Integer> quantityList = getQuantityList();
+
+        //Using The Items without duplicates
         for (Item item : removeDuplicateItem(items)) {
+            //Resetting item price and quantity for new item
             itemPrice = 0F;
-            itemCount = quantityList.get(item.getSKU());
+            itemQuantity = quantityList.get(item.getSKU());
             //Sort The Special Offers List To The Highest First （So The Cheapest Deal Is Reached)
             List<Item.SpecialPrice> sortedUsers = item.getSpecialPriceList().stream()
                     .sorted(Comparator.comparing(Item.SpecialPrice::getSpecialPriceQuantity).reversed())
                     .collect(Collectors.toList());
 
-            while (itemCount > 0) {
+            //(iteration) until every item has been processed
+            while (itemQuantity > 0) {
+                //Check if item has a special price
                 if (!item.getSpecialPriceList().isEmpty()) {
+                    //Process each special price
                     for (Item.SpecialPrice specialPrice : sortedUsers) {
-
-                        if (itemCount - maxValue >= 0) {
-                            maxValue = specialPrice.getSpecialPriceQuantity();
+                        //Check if the quantity of items is more or the same as the special price quantity
+                        if (itemQuantity - specialPriceQuantityNeeded >= 0) {
+                            //change the quantity needed as it meets requirements
+                            specialPriceQuantityNeeded = specialPrice.getSpecialPriceQuantity();
+                            //update price
                             itemPrice = itemPrice + specialPrice.getSpecialPrice();
-                            itemCount = itemCount - maxValue;
-                        } else if (itemCount > 0) {
+                            //remove quantity from item count
+                            itemQuantity = itemQuantity - specialPriceQuantityNeeded;
+                        }
+                        //if it doesn't meet the requirements
+                        else if (itemQuantity > 0) {
                                 itemPrice = itemPrice + item.getSKUPrice();
-                                itemCount = itemCount - 1;
+                                itemQuantity = itemQuantity - 1;
                         }
                     }
                 }
+                //no special prices
                 else {
                     itemPrice = itemPrice + item.getSKUPrice();
-                    itemCount = itemCount - 1;
+                    itemQuantity = itemQuantity - 1;
                 }
             }
-
+            //add to stats for print out
             totalPriceWithOutSpecialPrice = totalPriceWithOutSpecialPrice + (item.getSKUPrice() * quantityList.get(item.getSKU()));
             totalPriceWithSpecialPrice = totalPriceWithSpecialPrice + itemPrice;
 
             //CHANGE THIS TO SHOW WHAT WAS CHARGE
+
+            //print out the item and special offer prices
             sb.append("\nSKU = " + item.getSKU() + "\n Quantity = " + quantityList.get(item.getSKU()) + "\n Price Per Item = " + dollarFormat.format(itemPrice) + "\n Special Offers = " + sortedUsers);
         }
 
+        //print out checkout details
         sb.append("\n      ===========================" + "\n           *** Checkout ****" + "\n      ===========================");
         sb.append("\n        Amount W/O    : " + dollarFormat.format(totalPriceWithOutSpecialPrice));
 
